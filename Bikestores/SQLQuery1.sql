7aae246e6b56a77d18c6ead485e89ab7d3917f7a
Shivam Gupta <sgs.shivam99@outlook.com>
2025-01-31 12:19:57 +0530

    commit

--- Bikestores/SQLQuery1.sql
+++ Bikestores/SQLQuery1.sql
@@ -1,12 +1,5 @@
 --============================================================MS SQLSERVER TRAINING=====================================================================================
 
------create database 'sample_db'
-create database BikeStores;
------alter database(if needed) 'sample_db' to 'BikeStores'
-alter  database sample_db modify name=BikeStores;
-------use database 'sample_db'
-use BikeStores;
------Create schema
 
 
 
